c6dc2e12dab5e5871b3c5250c47d722f5afe40db
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-06 17:04:26 -0500

    fix the handle keys shift to move letters left to right once it hits 15

--- src/hooks/useKeysPressed/useKeysPressed.ts
+++ src/hooks/useKeysPressed/useKeysPressed.ts
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 
 export default function useKeysPressed() {
-    const [keysPressed, setKeysPressed] = useState<string[]>([]);
+    const [ keysPressed, setKeysPressed ] = useState<string[]>([]);
 
     const handleKeyPress = ({ key }: KeyboardEvent) => {
-        const clone = [...keysPressed];
-        const previousKeysPressed = keysPressed.length > 14 ? [...clone.splice(0, clone.length - 1)] : keysPressed;
-        previousKeysPressed.unshift(key);
-        setKeysPressed([...previousKeysPressed]);
+        const clone = [ ...keysPressed ];
+        
+        if (clone.length > 14) {
+            clone.shift();
+            clone.unshift(key);
+            clone.shift();
+        } else {
+            clone.push(key);
+        }
+
+        setKeysPressed(clone);
     };
 
     useEffect(() => {
