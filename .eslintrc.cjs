0867390f6474026cc7953944db62d9dc1cd78a1e
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-06 16:26:49 -0500

    add array bracket spacing linter rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,16 +9,17 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'react'],
+  plugins: [ 'react-refresh', 'react' ],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'jsx-quotes': ['error', 'prefer-single'],
-    'object-curly-spacing': ['error', 'always'],
-    'react/jsx-curly-spacing': ['error', { 'when': 'always', 'children': true }],
-    'react/react-in-jsx-scope': [0, 'never'],
-    'react/jsx-curly-brace-presence': ['error', 'always'],
-    'react/jsx-space-before-closing': ['error', 'always']
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'always' ],
+    'jsx-quotes': [ 'error', 'prefer-single' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'react/jsx-curly-spacing': [ 'error', { 'when': 'always', 'children': true } ],
+    'react/react-in-jsx-scope': [ 0, 'never' ],
+    'react/jsx-curly-brace-presence': [ 'error', 'always' ],
+    'react/jsx-space-before-closing': [ 'error', 'always' ]
   }
 };
